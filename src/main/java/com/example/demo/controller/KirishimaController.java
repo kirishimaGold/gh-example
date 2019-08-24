83e99e3c279b5d9a7fcd9bfe057ccb38871bcf5a
きりしま <kiri.linksite@gmail.com>
2019-08-24 13:59:51 +0900

    追加

--- src/main/java/com/example/demo/controller/KirishimaController.java
+++ src/main/java/com/example/demo/controller/KirishimaController.java
@@ -1,22 +1,27 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.dto.AudienceRatingDto;
 import com.example.demo.service.DbMockService;
 
 @Controller
-public class JstlStudyController {
+public class KirishimaController {
 
     @Autowired
     private DbMockService dbMockService;
 
-    @RequestMapping(path = "/jstl-study")
+    @RequestMapping(path = "/audienceRating")
     public String index(HttpServletRequest request) {
 
-        return "JstlStudy";
+        ArrayList<AudienceRatingDto> result = dbMockService.read();
+        request.setAttribute("audienceRatingDtoList", result);
+        return "AudienceRating";
     }
 }
---
+++ src/main/java/com/example/demo/dto/ContactDto.java
@@ -0,0 +1,77 @@
+package com.example.demo.dto;
+
+public class ContactDto {
+    private String title;
+    private String content;
+    private String ungency;
+    private String limit;
+    private String drrafter;
+    private String status;
+    private String responser;
+    private String category;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUngency() {
+        return ungency;
+    }
+
+    public void setUngency(String ungency) {
+        this.ungency = ungency;
+    }
+
+    public String getLimit() {
+        return limit;
+    }
+
+    public void setLimit(String limit) {
+        this.limit = limit;
+    }
+
+    public String getDrrafter() {
+        return drrafter;
+    }
+
+    public void setDrrafter(String drrafter) {
+        this.drrafter = drrafter;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getResponser() {
+        return responser;
+    }
+
+    public void setResponser(String responser) {
+        this.responser = responser;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+}
